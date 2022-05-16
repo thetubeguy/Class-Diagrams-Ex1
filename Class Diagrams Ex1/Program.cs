b21d675764fc3661241e4e397dc31786b484d848
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-05-16 20:15:30 +0100

    Add project files.

---
+++ Class Diagrams Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Class_Diagrams_Ex1;
+
+Student Adrian = new("Hallas", 3, 'Q');
+
+Console.WriteLine($"Adrian's grade is {Adrian.Grade}");
+Adrian.upgrade();
+Adrian.downgrade();
+
---
+++ Class Diagrams Ex1/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Diagrams_Ex1
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+
+        int group;
+        public int Group{
+            get { return group; }
+            private set
+            {
+                if((value >=1) && (value <=5))
+                    group = value;
+            }
+        }
+        static readonly char[] charlist = new char[] {'A','B','C','D','E','F'};
+        
+        char grade;
+        public char Grade {
+            get { return grade; } 
+            private set {
+                if (charlist.Contains(value))
+                {
+                    grade = value;
+                }
+                
+            } 
+        }
+        
+        private string secretNickName = "MySecretNickName";
+
+        public Student(string name, int group, char grade)
+        {
+            Name = name;
+            Group = group;
+            Grade = grade;
+        }
+
+        public void upgrade()
+        {
+            int position = Array.IndexOf(charlist, grade);
+            if(position != (charlist.Length)-1)  Grade = charlist[position + 1];
+        }
+
+        public void downgrade()
+        {
+            int position = Array.IndexOf(charlist, grade);
+            if (position != 0) Grade = charlist[position - 1];
+
+        }
+
+    }
+}
